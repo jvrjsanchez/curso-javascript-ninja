dbd8189af34aefb814ed93cb36935889f3fb8c79
jvrjsanchez <javiersanchez.cna@gmail.com>
2026-09-10 23:30:16 -0500

    added picanha

--- challenge-01/challenge-01.js
+++ challenge-01/challenge-01.js
@@ -22,7 +22,7 @@ soma = 72;
 var souninja = True;
 
 // Declare uma variável chamada `comida` que recebe um array com os valores 'arroz', 'feijão' e 'ovo'.
-var comida = ["arroz", "feijao", "carne de sol"];
+var comida = ["arroz", "feijao", "picanha"];
 
 // Digite a instrução que imprime o valor de 'feijao', que está na variável `comida`.
 console.log(comida[1]);
